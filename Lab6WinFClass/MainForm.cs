b6487793f50df826db82e46a719c4a521251a29a
Mercrual <66302410+Mercrual@users.noreply.github.com>
2021-03-01 12:45:37 +0500

    Add files via upload

---
+++ Lab6WinFClass/MainForm.cs
@@ -0,0 +1,67 @@
+﻿using Lab6WinFClass.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab6WinFClass
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+            private void BtnInfProduct_Click(object sender, EventArgs e)
+            {
+                //заполнение свойств экемпляра класса Shop
+                product.IdProduct = Convert.ToInt32(NumUDId.Value);
+                product.NameProduct = TxtBoxNameProduct.Text;
+                product.Provider = TxtBoxProvider.Text;
+                product.CountProduct =
+               Convert.ToInt32(NumUDCountProduct.Value);
+                product.PriceProduct =
+               Convert.ToDouble(NumUDPriceProduct.Value);
+                //извлечение значение изсвойства
+                MessageBox.Show(product.InfProduct);
+            }
+
+        private void BtnSumProduct_Click(object sender, EventArgs e)
+        {
+                //заполнение свойств экемпляра класса Shop
+                product.IdProduct = Convert.ToInt32(NumUDId.Value);
+ product.NameProduct = TxtBoxNameProduct.Text;
+                product.Provider = TxtBoxProvider.Text;
+                product.CountProduct =
+               Convert.ToInt32(NumUDCountProduct.Value);
+                product.PriceProduct =
+               Convert.ToDouble(NumUDPriceProduct.Value);
+                //извлечение значение изсвойства
+                MessageBox.Show(product.SumProduct().ToString());
+            }
+        }
+    }
+    namespace Lab6WinFClass
+    {
+        public partial class MainForm : Form
+        {
+            //создание экземпляра класса Shop
+            private Shop product = new Shop();
+        }
+    }
---
+++ Lab6WinFClass/Models/Shop.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6WinFClass.Models
+{
+ 
+        class Shop
+        {
+            public int IdProduct { set; get; }
+            //{ set; get; } означает что полю можно задать значение
+            //и получить значение соответственно
+            public String NameProduct { set; get; }
+            public String Provider { set; get; }
+            public int CountProduct { set; get; }
+            public double PriceProduct { set; get; }
+            public String InfProduct
+            {
+                get
+                {
+                    return String.Format("Продукт {0} доставлен поставщиком {1}",
+                NameProduct, Provider);
+                }
+            }
+            public double SumProduct()
+            {
+                return CountProduct * PriceProduct;
+            }
+        }
+    }
+
